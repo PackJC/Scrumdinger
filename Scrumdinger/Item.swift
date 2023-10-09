8fc583fca173ce7a4273f140561e2dc454cc529c
Cole <johnnycpack@gmail.com>
2023-10-09 10:47:23 -0400

    Initial Commit

---
+++ Scrumdinger/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Scrumdinger
+//
+//  Created by Johnny Pack on 10/9/23.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,32 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Johnny Pack on 10/9/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ScrumdingerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}

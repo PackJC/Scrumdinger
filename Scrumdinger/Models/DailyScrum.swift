67cb86586d0a55d1ecfe81ed17e0aed859afe028
Cole <johnnycpack@gmail.com>
2023-10-13 09:33:31 -0400

    Displaying Data in List

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -14,9 +14,15 @@ struct DailyScrum: Identifiable {
     var attendees: [String]
     var lengthInMinutes: Int
     var theme: Theme
+
+init(id: UUID = UUID(), title: String, attendees: [String], lengthInMinutes: Int, theme: Theme) {
+      self.id = id
+      self.title = title
+      self.attendees = attendees
+      self.lengthInMinutes = lengthInMinutes
+      self.theme = theme
+  }
 }
-
-
 extension DailyScrum {
     static let sampleData: [DailyScrum] =
     [
